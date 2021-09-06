36e2ecedfaebaa22c9383c79b98a73157036aabe
flash <2morrowMan@gmail.com>
2021-09-06 10:42:02 +0300

    Initial commit

---
+++ Assets/EditorHelpers/Utils/EditorButtonAttribute.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorHelpers
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class EditorButtonAttribute : Attribute
+	{
+		public readonly ButtonMode mode = ButtonMode.AlwaysEnabled;
+
+		public EditorButtonAttribute() { }
+
+		public EditorButtonAttribute(ButtonMode mode)
+		{
+			this.mode = mode;
+		}
+	}
+
+	public enum ButtonMode
+	{
+		AlwaysEnabled,
+		EnabledInPlayMode,
+		DisabledInPlayMode
+	}
+
+	#if UNITY_EDITOR
+	[CanEditMultipleObjects]
+	[CustomEditor( typeof(UnityEngine.Object), true )]
+	public class EditorButtonEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			var targetType = target.GetType();
+			var methods = targetType
+				.GetMethods( BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance )
+				.Where( m => m.GetParameters().Length == 0 );
+
+			// Loop through all methods with no parameters
+			foreach (var method in methods)
+			{
+				var attributes = method.GetCustomAttributes( typeof(EditorButtonAttribute), true );
+				if (attributes.Length > 0)
+				{
+					var attr = (EditorButtonAttribute) attributes[0];
+					// Determine whether the button should be enabled based on its mode
+					GUI.enabled = attr.mode == ButtonMode.AlwaysEnabled
+								|| (EditorApplication.isPlaying
+									? attr.mode == ButtonMode.EnabledInPlayMode
+									: attr.mode == ButtonMode.DisabledInPlayMode);
+
+					// Draw a button which invokes the method
+					if (GUILayout.Button( ObjectNames.NicifyVariableName( method.Name ) ))
+					{
+						foreach (var script in targets)
+						{
+							method.Invoke( script, null );
+							
+							if (!Application.isPlaying)
+								EditorUtility.SetDirty( script );
+						}
+					}
+
+					GUI.enabled = true;
+				}
+			}
+
+			// Draw the rest of the inspector as usual
+			DrawDefaultInspector();
+		}
+	}
+	#endif
+}
---
+++ Assets/EditorHelpers/Utils/ShowOnlyAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[AttributeUsage( AttributeTargets.Field | AttributeTargets.Property, Inherited = true, AllowMultiple = false )]
+public class ShowOnlyAttribute : PropertyAttribute
+{
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer( typeof(ShowOnlyAttribute) )]
+public class ShowOnlyDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+	{
+		string valueStr;
+
+		switch (prop.propertyType)
+		{
+			case SerializedPropertyType.Integer:
+				valueStr = prop.intValue.ToString();
+				break;
+			case SerializedPropertyType.Boolean:
+				valueStr = prop.boolValue.ToString();
+				break;
+			case SerializedPropertyType.Float:
+				valueStr = prop.floatValue.ToString( "0.00000" );
+				break;
+			case SerializedPropertyType.String:
+				valueStr = prop.stringValue;
+				break;
+			case SerializedPropertyType.Enum:
+				valueStr = prop.enumDisplayNames[prop.enumValueIndex];
+				break;
+			default:
+				valueStr = "(not supported type)";
+				break;
+		}
+
+		EditorGUI.LabelField( position, label.text, valueStr );
+	}
+}
+#endif
